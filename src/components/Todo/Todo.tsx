5ba189b3dce6698971116bd45173d472986d611b
Mccary.Rob <Mccary.Rob@doc.sc.gov>
2022-06-23 09:16:41 -0400

    Added edit capability

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useState} from 'react'
 import './Todo.scss'
 import { TodoModel } from '../../models/TodoModel'
 import {RiDeleteBin5Line} from 'react-icons/ri'
@@ -8,12 +8,15 @@ interface Props {
   index: number;
   id: number;
   todo: string;
-  isCompleted?: boolean;
+  isCompleted: boolean;
   todos: TodoModel[];
   setTodos: React.Dispatch<React.SetStateAction<TodoModel[]>>
 }
 
 const Todo: React.FC<Props> = ({index, todo, isCompleted, id, todos, setTodos}) => {
+
+  const [edit, setEdit] = useState<boolean>(false)
+  const [editTodoInput, setEditTodoInput] = useState<string>(todo)
 
   const deleteTodo = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id))
@@ -24,6 +27,14 @@ const Todo: React.FC<Props> = ({index, todo, isCompleted, id, todos, setTodos}) 
   )
   }
 
+  const editTodo = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => todo.id === id ? {...todo, todo: editTodoInput} : todo)
+    )
+    setEdit(false);
+  }
+
 
   return (
     <div className='todo-container'>
@@ -31,14 +42,26 @@ const Todo: React.FC<Props> = ({index, todo, isCompleted, id, todos, setTodos}) 
         <p>{index+=1}</p>
       </div>
       <div className="todo-details">
-        <form>
-          {isCompleted ? <s>{todo}</s> : 
-          <input value={todo} />}
-          
+        <form onSubmit={(e) => editTodo(e, id)}>
+          {edit ? (
+            <input 
+              value={editTodoInput}
+              onChange={(e)=> setEditTodoInput(e.target.value)}
+              />
+            )  :
+          isCompleted ? <s>{todo}</s> : 
+          <input value={todo} />
+          }
         </form>
       </div>     
       <div className='crud-icons'>
-        <button><AiTwotoneEdit size={28}/></button>
+        <button onClick={() => {
+          if (!edit && !isCompleted) {
+          setEdit(!edit)
+          }
+          }}>
+            <AiTwotoneEdit size={28}/>
+        </button>
         <button onClick={() => deleteTodo(id)}><RiDeleteBin5Line size={28}/></button>
         <button onClick={() => completeTodo(id)}><AiOutlineCheck size={28}/></button>
       </div>
